ed23caa97a18a413d8c35b1347ecf12a92abea25
hariiiiiiiiiiii12 <harimsdian07@gmail.com>
2025-12-23 08:19:57 +0530

    initial react project setup

---
+++ react-rtk-demo/features/icecream/IceCreamView.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const IceCreamView = () => {
+  return (
+    <div>
+      <h2>Number of Ice creams - </h2>
+      <button>Order Ice cream</button>
+      <button>Restock Ice creams</button>
+    </div>
+  );
+};
---
+++ react-rtk-demo/features/user/UserView.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const UserView = () => {
+  return <div>
+    <h2>List of Users</h2>
+  </div>;
+};
